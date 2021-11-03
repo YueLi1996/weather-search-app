c94c0e639c27473f0d2eef2ad84a41269a6ecd2e
YueLi1996 <58449011+YueLi1996@users.noreply.github.com>
2021-11-03 12:16:32 -0700

    index.html is done

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+ 
+app.set('views', path.join(__dirname))
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.listen(3000, ()=>{
+  console.log("Serving on PORT 3000")
+})
+
+ 
