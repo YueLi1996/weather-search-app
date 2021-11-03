de924342a2357c2404d125eb8a8ad02b70a72db9
YueLi1996 <58449011+YueLi1996@users.noreply.github.com>
2021-11-03 12:34:39 -0700

    images updated

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
- 
+
+app.set('view engine', '?????')
 app.set('views', path.join(__dirname))
 
 app.get('/', function (req, res) {
