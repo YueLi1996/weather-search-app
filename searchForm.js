9859b2d58b1bb1d6a02424677e26e9fa282b3901
YueLi1996 <58449011+YueLi1996@users.noreply.github.com>
2021-11-03 21:10:05 -0700

    need CSS changes

--- searchForm.js
+++ searchForm.js
@@ -3,23 +3,29 @@ var cityDOM = document.getElementById("city")
 var stateDOM = document.getElementById("state")
 var searchbtnDOM = document.getElementById("searchbtn")
 
+// Enable submit button when info is filled
+if (streetDOM.value != "" || cityDOM.value !== "" || stateDOM.value != "default") {
+    console.log("????") 
+    searchbtnDOM.disabled = ""
+}
+
 // handle checkbox of current location
 var checkbox = document.querySelectorAll('input[type="checkbox"]')[0];
 checkbox.addEventListener("click", () => {
     if(checkbox.checked) {
         if(streetDOM.value != "" || cityDOM.value != "") {
-            streetDOM.value = "";
-            cityDOM.value = "";
+            streetDOM.value = ""
+            cityDOM.value = ""
         }
-        streetDOM.disabled = "true";
-        cityDOM.disabled = "true";
+        streetDOM.disabled = "true"
+        cityDOM.disabled = "true"
         stateDOM.value = "default"
-        stateDOM.disabled = "true";
+        stateDOM.disabled = "true"
     }
     else {
-        streetDOM.disabled = "";
-        cityDOM.disabled = "";
-        stateDOM.disabled = "";
+        streetDOM.disabled = ""
+        cityDOM.disabled = ""
+        stateDOM.disabled = ""
     }
 })
 
@@ -30,8 +36,3 @@ clearButton.addEventListener("click", () => {
     cityDOM.value = ""
     stateDOM.value = "default"
 })
-
-// Enable submit button when info is filled
-if (streetDOM.value != "" || cityDOM.value != "" || stateDOM.value != "default") {
-    searchbtnDOM.disabled = ""
-}
