bff8aa35a52c6d99c1af9ff139b7284a77456e46
YueLi1996 <58449011+YueLi1996@users.noreply.github.com>
2021-11-03 20:49:37 -0700

    figure out basic logic of search form

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 
-app.set('view engine', '?????')
-app.set('views', path.join(__dirname))
-
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.render('index')
 })
 
@@ -13,4 +9,3 @@ app.listen(3000, ()=>{
   console.log("Serving on PORT 3000")
 })
 
- 
---
+++ searchForm.js
@@ -0,0 +1,37 @@
+var streetDOM = document.getElementById("street")
+var cityDOM = document.getElementById("city")
+var stateDOM = document.getElementById("state")
+var searchbtnDOM = document.getElementById("searchbtn")
+
+// handle checkbox of current location
+var checkbox = document.querySelectorAll('input[type="checkbox"]')[0];
+checkbox.addEventListener("click", () => {
+    if(checkbox.checked) {
+        if(streetDOM.value != "" || cityDOM.value != "") {
+            streetDOM.value = "";
+            cityDOM.value = "";
+        }
+        streetDOM.disabled = "true";
+        cityDOM.disabled = "true";
+        stateDOM.value = "default"
+        stateDOM.disabled = "true";
+    }
+    else {
+        streetDOM.disabled = "";
+        cityDOM.disabled = "";
+        stateDOM.disabled = "";
+    }
+})
+
+// handle clear button
+var clearButton = document.getElementById('clearbtn')
+clearButton.addEventListener("click", () => {
+    streetDOM.value = ""
+    cityDOM.value = ""
+    stateDOM.value = "default"
+})
+
+// Enable submit button when info is filled
+if (streetDOM.value != "" || cityDOM.value != "" || stateDOM.value != "default") {
+    searchbtnDOM.disabled = ""
+}
